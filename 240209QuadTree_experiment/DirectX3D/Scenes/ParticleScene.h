1d40b7da0e12db913d83f13cbcb34f45454cdb11
schr-dinger <138895829+schr-dinger@users.noreply.github.com>
2024-02-21 12:39:28 +0900

    height map test sln

---
+++ 240209QuadTree_experiment/DirectX3D/Scenes/ParticleScene.h
@@ -0,0 +1,25 @@
+#pragma once
+class ParticleScene : public Scene
+{
+public:
+    ParticleScene();
+    ~ParticleScene();
+
+    virtual void Update() override;
+    virtual void PreRender() override;
+    virtual void Render() override;
+    virtual void PostRender() override;
+    virtual void GUIRender() override;
+
+private:
+
+    //Particle* particle;
+    ParticleSystem* particle;
+
+    SphereCollider* collider;
+
+    ModelAnimator* Penguin;
+
+    Transform* A[2];
+};
+
