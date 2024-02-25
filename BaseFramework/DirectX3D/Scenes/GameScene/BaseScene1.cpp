9f7bc9d65565ac163ee162df2eae8b04d32a7438
schr-dinger <janghyunsik@naver.com>
2024-02-25 19:23:05 +0900

    New Cam settings

--- BaseFramework/DirectX3D/Scenes/GameScene/BaseScene1.cpp
+++ BaseFramework/DirectX3D/Scenes/GameScene/BaseScene1.cpp
@@ -18,7 +18,7 @@ BaseScene1::BaseScene1()
 	player->SetTerrain(terrainF);
 
 	CAM->SetTarget(player);
-	CAM->TargetOptionLoad("test2"); // 나루토에 맞춘 카메라 위치 설정 로드 (있으면 로드 없으면 그대로)
+	CAM->TargetOptionLoad("test3");
 	//CAM->LookAtTarget(); // 팔로우캠 + 추적 대상 있음 + 그 추적 대상을 락온으로 추적 (이 경우는 나루토)
 
 	PalsManager::Get()->SetTarget(player);
