44c09becb8de309e995800b40e9ab3935071dd4b
schr-dinger <janghyunsik@naver.com>
2024-03-10 11:38:42 +0900

    Update Penguin.cpp

--- BaseFramework/DirectX3D/Objects/Character/Penguin.cpp
+++ BaseFramework/DirectX3D/Objects/Character/Penguin.cpp
@@ -379,38 +379,38 @@ void Penguin::SetPath()
 
     AStarManager::Get()->GetAStar()->GetPath(startIndex, endIndex, path); // 길을 낸 다음 path 벡터에 저장
 
-    AStarManager::Get()->GetAStar()->MakeDirectionPath(transform->GlobalPos(), target->GlobalPos(), path); // 장애물을 지우고 path에 덮어씌우기
+    //AStarManager::Get()->GetAStar()->MakeDirectionPath(transform->GlobalPos(), target->GlobalPos(), path); // 장애물을 지우고 path에 덮어씌우기
 
-    UINT pathSize = path.size(); // 처음 나온 경로 벡터 크기를 저장
+    //UINT pathSize = path.size(); // 처음 나온 경로 벡터 크기를 저장
 
-    while (path.size() > 2) // "남겨진" 경로 노드가 1군데 이하가 될 때까지
-    {
-        vector<Vector3> tempPath = path; // 계산용 임시 경로 받아오기
-        tempPath.erase(tempPath.begin()); // 최종 목적지 지우기 (장애물이 있었기 때문에 지금은 중간을 가는 중)
-        tempPath.pop_back(); // 시작 위치도 생각에서 제외하기 (이미 지나가고 있으니까)
+    //while (path.size() > 2) // "남겨진" 경로 노드가 1군데 이하가 될 때까지
+    //{
+    //    vector<Vector3> tempPath = path; // 계산용 임시 경로 받아오기
+    //    tempPath.erase(tempPath.begin()); // 최종 목적지 지우기 (장애물이 있었기 때문에 지금은 중간을 가는 중)
+    //    tempPath.pop_back(); // 시작 위치도 생각에서 제외하기 (이미 지나가고 있으니까)
 
-        // 최종과 시작이 빠진 경로의 새로운 시작과 끝을 내기
-        Vector3 start = path.back();
-        Vector3 end = path.front();
+    //    // 최종과 시작이 빠진 경로의 새로운 시작과 끝을 내기
+    //    Vector3 start = path.back();
+    //    Vector3 end = path.front();
 
-        //다시 나온 경로에서 장애물 계산을 또 계산
-        AStarManager::Get()->GetAStar()->MakeDirectionPath(start, end, tempPath);
+    //    //다시 나온 경로에서 장애물 계산을 또 계산
+    //    AStarManager::Get()->GetAStar()->MakeDirectionPath(start, end, tempPath);
 
-        //계산 결과 피드백
-        path.clear();
-        path = tempPath;
+    //    //계산 결과 피드백
+    //    path.clear();
+    //    path = tempPath;
 
-        //경로 벡터에 새로운 시작과 끝을 포함
-        path.insert(path.begin(), end);
-        path.push_back(start);
+    //    //경로 벡터에 새로운 시작과 끝을 포함
+    //    path.insert(path.begin(), end);
+    //    path.push_back(start);
 
-        // 계산을 다시 했는데 벡터 크기가 그대로라면 = 길이 바뀌지 않았다
-        if (pathSize == path.size()) break; // 이 이상 계산을 계속할 이유가 없다
-        else pathSize = path.size(); // 줄어든 경로 계산을 반영을 해주고 반복문을 다시 진행
-    }
+    //    // 계산을 다시 했는데 벡터 크기가 그대로라면 = 길이 바뀌지 않았다
+    //    if (pathSize == path.size()) break; // 이 이상 계산을 계속할 이유가 없다
+    //    else pathSize = path.size(); // 줄어든 경로 계산을 반영을 해주고 반복문을 다시 진행
+    //}
 
-    // 다시 조정된, 내가 갈 수 있는 경로에, 최종 목적지를 다시 한번 추가한다
-    path.insert(path.begin(), target->GlobalPos());
+    //// 다시 조정된, 내가 갈 수 있는 경로에, 최종 목적지를 다시 한번 추가한다
+    //path.insert(path.begin(), target->GlobalPos());
 
 }
 
