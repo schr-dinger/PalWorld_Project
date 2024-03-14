89fe1b92ddf2848974848d45cb92520ed1a3b4a2
gyro1515 <gyro1515@naver.com>
2024-03-14 21:08:13 +0900

    라이트 수정

--- BaseFramework/DirectX3D/Shaders/Light/Light.hlsl
+++ BaseFramework/DirectX3D/Shaders/Light/Light.hlsl
@@ -35,7 +35,8 @@ float4 PS(LightPixelInput input) : SV_TARGET
     //return CalcLights(input) * 0.5f + diffuseMap.Sample(samp, input.uv) * 0.5f;
     //return CalcLights(input) * 0.3f + diffuseMap.Sample(samp, input.uv) * 0.7f;
     //return CalcLights(input) * 0.2f + diffuseMap.Sample(samp, input.uv) * 0.8f;
-    return CalcLights(input) * 0.1f + diffuseMap.Sample(samp, input.uv) * 0.9f;
+    //return CalcLights(input) * 0.1f + diffuseMap.Sample(samp, input.uv) * 0.9f;
+	return CalcLights(input) * 0.5f + diffuseMap.Sample(samp, input.uv) * 0.8f;
 
 
 }
