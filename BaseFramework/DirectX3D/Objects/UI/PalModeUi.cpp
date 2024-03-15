efef25bb11cff908cd916987d47b9db2da650e93
schr-dinger <janghyunsik@naver.com>
2024-03-15 23:22:18 +0900

    Finished Mode and moveP

--- BaseFramework/DirectX3D/Objects/UI/PalModeUi.cpp
+++ BaseFramework/DirectX3D/Objects/UI/PalModeUi.cpp
@@ -88,6 +88,7 @@ void PalModeUi::PostRender()
 
 void PalModeUi::GuiRender()
 {
+
 }
 
 void PalModeUi::SetTexture()
--- BaseFramework/DirectX3D/Scenes/GameScene/BaseScene1.cpp
+++ BaseFramework/DirectX3D/Scenes/GameScene/BaseScene1.cpp
@@ -164,7 +164,7 @@ void BaseScene1::GUIRender()
 	//palBox->GUIRender();
 
 	//PalsManager::Get()->GUIRender();
-	//PlayerPalsManager::Get()->GUIRender();
+	PlayerPalsManager::Get()->GUIRender();
 
 	PlayerManager::Get()->GUIRender();
 	//PalSpearManager::Get()->GUIRender();
