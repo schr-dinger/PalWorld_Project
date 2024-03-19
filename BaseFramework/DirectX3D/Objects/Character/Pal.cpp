94a85b65be9e0debaf0f8bb641b5cc46ac1ddc0b
Icetea04 <159400782+Icetea04@users.noreply.github.com>
2024-03-19 13:04:37 +0900

    Revert "Revert "Merge branch 'main' into Test3""
    This reverts commit 9f1ae62ce9cba0dfac7c6fdbccce8759863e5f06.

--- BaseFramework/DirectX3D/Objects/Character/Pal.cpp
+++ BaseFramework/DirectX3D/Objects/Character/Pal.cpp
@@ -11,12 +11,10 @@ Pal::Pal()
 	iconC = nullptr;
 	isUIOn = false;
 	onUITime = 0.0f;
-	shadowSphere = nullptr;
 }
 
 Pal::~Pal()
 {
-	delete shadowSphere;
 	delete skill[0];
 	delete skill[1];
 	delete skill[2];
