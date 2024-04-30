0855fc9f8054d847d51694fa82d1bb6b99cd9bf8
schr-dinger <janghyunsik@naver.com>
2024-04-30 20:21:07 +0900

    little bit of shader stuff test

--- BaseFramework/DirectX3D/Shaders/Model/ModelInstancing.hlsl
+++ BaseFramework/DirectX3D/Shaders/Model/ModelInstancing.hlsl
@@ -24,11 +24,14 @@ LightPixelInput VS(VertexInstancing input)
 float4 PS(LightPixelInput input) : SV_TARGET
 {
 	//return CalcLights(input);
-    //return CalcLights(input) * 0.5f + diffuseMap.Sample(samp, input.uv) * 0.5f;
-    //return CalcLights(input) * 0.3f + diffuseMap.Sample(samp, input.uv) * 0.7f;
+	//return CalcLights(input) * 0.5f + diffuseMap.Sample(samp, input.uv) * 0.5f;
+	//return CalcLights(input) * 0.3f + diffuseMap.Sample(samp, input.uv) * 0.7f;
 	//return CalcLights(input) * 0.2f + diffuseMap.Sample(samp, input.uv) * 0.8f;
-    //return CalcLights(input) * 0.1f + diffuseMap.Sample(samp, input.uv) * 0.9f;
-	return CalcLights(input) * 0.3f + diffuseMap.Sample(samp, input.uv) * 0.7f;
+	//return CalcLights(input) * 0.1f + diffuseMap.Sample(samp, input.uv) * 0.9f;
+	//return CalcLights(input) * 0.3f + diffuseMap.Sample(samp, input.uv) * 0.7f;
 
+	return  float4(CalcLights(input).xyz,0) + diffuseMap.Sample(samp, input.uv);
 
+	//return CalcLights(input) * 0.0f;
+	//return {0,0,0,1.0f};
 }
